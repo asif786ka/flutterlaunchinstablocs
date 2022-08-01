fec857094f58047a6ab60bf93b97c50e2e105962
asifmacmini <asif@thinkific.com>
2022-08-01 16:35:35 +0530

    Connected login cubit.

--- lib/screens/login/login_cubit.dart
+++ lib/screens/login/login_cubit.dart
@@ -21,7 +21,7 @@ class LoginCubit extends Cubit<LoginState> {
   }
 
   void logInWithCredentials() async {
-    if (state.isFormValid || state.status == LoginStatus.submitting) return;
+    if (!state.isFormValid || state.status == LoginStatus.submitting) return;
     emit(state.copyWith(status: LoginStatus.submitting));
     try {
       await _authRepository.logInWithEmailAndPassword(
