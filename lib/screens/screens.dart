b847fe0b183a3517d15176805b6d96e0389dd4a3
asifmacmini <asif@thinkific.com>
2022-08-04 15:00:27 +0530

    Added edit profile cubit.

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -2,3 +2,4 @@ export 'splash/splash_screen.dart';
 export 'login/login_screen.dart';
 export 'nav/nav_screen.dart';
 export 'signup/signup_screen.dart';
+export 'edit_profile/edit_profile_screen.dart';
